2af0ece4a6f2649bd6ace4d255ab769fe53d5695
ronny <you@example.com>
2022-05-07 20:51:08 +0200

    env2

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bashio
-
+echo $(bashio::services mqtt "host")
+echo $(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
 echo "Hello world!"
 
 python3 -m http.server 8000
