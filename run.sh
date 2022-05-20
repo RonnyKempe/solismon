445f55491e2f9140bea869ad0f15fa3d858d93a5
ronny <you@example.com>
2022-05-20 16:46:37 +0200

    1.0.6 Test Implement script

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/with-contenv bashio
-echo $(bashio::services mqtt "host")
-echo $(bashio::services mqtt "username")
+set -e
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT USERNAME=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
-echo "Hello world!"
+MQTT_SERVER_IP=$(bashio::config 'MQTT_SERVER_IP')
+MQTT_SERVER_USERNAME=$(bashio::config 'MQTT_SERVER_USERNAME')
+MQTT_SERVER_PASSWORD=$(bashio::config 'MQTT_SERVER_PASSWORD')
+MQTT_PORT=$(bashio::config 'MQTT_PORT')
+MQTT_TOPIC=$(bashio::config 'MQTT_TOPIC')
+MQTT_SERVER_ENABLE=$(bashio::config 'MQTT_SERVER_ENABLE')
+IP_INVERTER1=$(bashio::config 'IP_INVERTER1')
+INVERTER1_DONGLE_SERIAL=$(bashio::config 'INVERTER1_DONGLE_SERIAL')
+IP_INVERTER2=$(bashio::config 'IP_INVERTER2')
+INVERTER2_DONGLE_SERIAL=$(bashio::config 'INVERTER2_DONGLE_SERIAL')
 
-python3 -m http.server 8000
+echo "Solis MQTT Logger"
+echo $MQTT_HOST
+bashio::log.info "Inv1 IP "$INVERTER1_DONGLE_SERIAL
+/rhi.sh
